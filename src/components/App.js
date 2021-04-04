d2d82e4bebb57f043356f1ebbfa7da072e351015
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2021-04-04 22:45:37 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
+  constructor(props) {
+    super(props);
+    this.state = {
+      para: false
+    };
+    this.hadleclick = this.handleclick.bind(this);
+  }
+  handleclick() {
+    console.log("clicked");
+    if (this.state.para === false) {
+      this.setState({ para: true });
     }
+  }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+
+        <button id="click" onClick={this.hadleclick}>
+          click
+        </button>
+        <p id="para">
+          {this.state.para
+            ? "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+            : ""}
+        </p>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
